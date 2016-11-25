f3cb5ae44c0de110b0d042f226ef8fcecd27548c
Shun Usami <shunusami@Shun-no-MacBook-Air.local>
2016-11-25 17:55:38 +0900

    add file_name_refactor.py

---
+++ file_name_refactor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import pandas as pd
+from pick import pick
+
+def get_csv_list():
+    directories = [os.path.join(freelancer_path, x) for x in os.listdir(freelancer_path)]
+    directories = [x for x in directories if os.path.isdir(x)]
+    files = []
+    for directory in directories:
+        files += [os.path.join(directory, x) for x in os.listdir(directory) if x.endswith('files_to_submit')]
+    return files
+
+def get_sound_files_list(path):
+    paths = [os.path.join(path,x) for x in os.listdir(path) if x.endswith('.mp3')]
+    return paths
+
+def rename_files(paths):
+    for path in paths:
+        directory = os.path.dirname(path)
+        file = os.path.basename(path)
+        file = file.lower()
+        newpath = os.path.join(directory, file)
+        os.rename(path, newpath)
+
+
+if __name__ == "__main__":
+    # 単語を抽出してくるファイルとエクスポート先のファイルを選択
+    title                           = 'Please choose the csv file to inspect acceptance.'
+    freelancer_path                 = 'freelancers'
+    options                         = get_csv_list()
+    option, index                   = pick(options, title)
+
+    sounds                          = get_sound_files_list(option)
+    rename_files(sounds)
